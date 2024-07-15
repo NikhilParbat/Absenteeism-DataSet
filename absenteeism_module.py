cf9311aaa873ac3af2e01ad7a0e0e491dee20037
Nikhil <nikhilparbat908@gmail.com>
2024-07-15 15:27:20 +0530

    Almost Completed Course

--- absenteeism_module.py
+++ absenteeism_module.py
@@ -35,16 +35,22 @@ class absenteeism_model():
                 self.reg = pickle.load(model_file)
                 self.scaler = pickle.load(scaler_file)
                 self.data = None
-        
+
+        def show_Columns(self):
+            if self.data is not None:
+                return self.preprocessed_data
+            
         def load_and_clean_data(self, data_file):
             
             df = pd.read_csv(data_file,delimiter=',')
             self.df_with_predictions = df.copy()
             df = df.drop(['ID'], axis = 1)
             df['Absenteeism Time in Hours'] = 'NaN'
- 
-            reason_columns = pd.get_dummies(df['Reason for Absence'], drop_first = True)
-            reason_columns =  reason_columns.replace({True: 1, False: 0})
+
+            reason_columns = pd.get_dummies(df['Reason for Absence'], drop_first=True)
+            reason_columns = reason_columns.replace({True: 1, False: 0})
+            # Explicitly call infer_objects to retain old behavior
+            reason_columns = reason_columns.infer_objects(copy=False)
             
             reason_type_1 = reason_columns.loc[:,1:14].max(axis=1)
             reason_type_2 = reason_columns.loc[:,15:17].max(axis=1)
@@ -96,6 +102,8 @@ class absenteeism_model():
             self.preprocessed_data = df.copy()
             
             self.data = self.scaler.transform(df)
+
+            df.head
     
         # a function which outputs the probability of a data point to be 1
         def predicted_probability(self):
